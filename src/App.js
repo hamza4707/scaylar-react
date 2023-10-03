0eb8bc8500cdf885b3b9887f60e887a5320ea736
hamza4707 <hamza.amjad4707@gmail.com>
2023-10-03 19:55:11 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layouts/Layout";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+      </Routes>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+
+function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://first-react-app-3eb72-default-rtdb.firebaseio.com/meetup.json"
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <section>Loading...</section>;
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+      {/* <ul>
+        {DUMMY_DATA.map((meetup) => {
+          return <li key={meetup.id}>{meetup.title}</li>;
+        })}
+      </ul> */}
+    </section>
+  );
+}
+
+export default AllMeetupsPage;

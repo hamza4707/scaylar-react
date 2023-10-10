5f5c0c9e08d149737e28ca0e3dd63ce850ef5ab2
hamza4707 <hamza.amjad4707@gmail.com>
2023-10-10 23:09:33 +0500

    [WIP redux-persist]

--- src/app/store.js
+++ src/app/store.js
@@ -1,21 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import favoriteReducer from "../features/favorite/favoriteSlice";
-// import storage from "redux-persist/lib/storage";
-// import { persistReducer, persistStore } from "redux-persist";
-// import thunk from "redux-thunk";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
 
-// const persistConfig = {
-//   key: "root",
-//   storage,
-// };
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-// const persistedReducer = persistReducer(persistConfig, favoriteReducer);
+const persistedReducer = persistReducer(persistConfig, favoriteReducer);
 
 export const store = configureStore({
-  reducer: { favoriteReducer },
-  // reducer: persistedReducer,
-  // devTools: process.env.NODE_ENV !== "production",
-  // middleware: [thunk],
+  // reducer: { favoriteReducer },
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
 
-// export const persistor = persistStore(store);
+export const persistor = persistStore(store);
